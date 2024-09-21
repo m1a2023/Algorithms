3cf8e2130267f25db20067c008dae871c845a2fe
m1a2023 <mag2017go@gmail.com>
2024-09-21 16:38:33 +0500

    class Constand was done; added comments

--- Algorithms/models/Algorithms/models/Constant.cs
+++ Algorithms/models/Algorithms/models/Constant.cs
@@ -9,13 +9,51 @@ using System.Threading.Tasks;
 
 namespace Algorithms.models.Algorithms.models
 {
-    /// <summary>Implementation Constant function algorithm</summary>
-    /// <typeparam name="T">Inner data type</typeparam>
-    public class Constant<T> : Algorithm<T>
-        where T : INumber<T>
-    {
-        /// <summary>general methond that executes algorithm</summary>
-        /// <param name="data">value collection, implemented via icollection</param>
-        public override void Execute(IList<T> data) { return; }
+	/// <summary>Implementation Constant function algorithm</summary>
+	/// <typeparam name="T">Inner data type</typeparam>
+	public class Constant<T> : Algorithm<T>
+		where T : INumber<T>
+	{
+		protected IList<T> Data { get; private set; }
+		protected T Result { get; private set; }
+
+		/// <summary>Default constructor</summary>
+		public Constant() { } 
+        
+		/// <summary>Extended constructor</summary>
+        /// <param name="Base"></param>
+        /// <param name="Exponent"></param>
+		public Constant(IList<T> Data)
+		{
+			this.Data = Data;
+			Result = T.One;
+		}
+
+		/// <summary>Additional method for extended constructor</summary>
+		/// <returns>Execution algorithm time in milliseconds</returns>
+		public decimal GetExecutionTime()
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			Execute(Data);
+
+			stopwatch.Stop();
+
+			return new decimal(stopwatch.Elapsed.TotalMilliseconds);
+		}
+
+		/// <summary>general methond that executes algorithm</summary>
+		/// <param name="data">value collection, implemented via icollection</param>
+		public override void Execute(IList<T> data) { return; }
+
+		/// <summary>String information representation</summary>
+		/// <returns>String of fields</returns>
+        public override string ToString()
+        {
+			string data = String.Join(", ", Data);
+			return $"Original collection: {data} [{Data.GetType()}]\n" +
+				$"Result: {Result} [{Result.GetType()}]\n" +
+				$"Execution time: {GetExecutionTime()}";
+        }
     }
 }
--- AlgorithmsTests/ConstantTests.cs
+++ AlgorithmsTests/ConstantTests.cs
@@ -8,48 +8,50 @@ using Algorithms.models.Algorithms.models;
 
 namespace AlgorithmsTests
 {
-    [TestClass]
-    public class ConstantTests
-    {
-        Constant<BigInteger> constantInt = new();
-        Constant<Double> constantDouble = new();
+	[TestClass]
+	public class ConstantTests
+	{
+		Constant<Int64> constantInt = new();
+		Constant<Double> constantDouble = new();
 
-        [TestMethod]
-        public void ArrayTest()
-        {
-            /**
-             *  Test integer array
-             */
-            BigInteger[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 90 };
+		[TestMethod]
+		public void ArrayTest()
+		{
+			/**
+			 *  Test integer array
+			 */
+			Int64[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 90 };
 
-            /**
-             *  
-             */
-            BigInteger one = BigInteger.One;   
+			/**
+			 *  
+			 */
+			BigInteger one = BigInteger.One;   
 
-            /**
-             *  Output 
-             */
-            Console.WriteLine("Execution " + array.GetType().Name + " array time: " + constantInt.GetExecutionTime(array));
-        }
+			/**
+			 *  Output 
+			 */
+			Console.WriteLine("Execution " + array.GetType().Name + " array time: " + constantInt.GetExecutionTime(array));
 
-        [TestMethod]
-        public void ListTest()
-        {
-            /**
-             *  Test Double list 
-             */
-            List<Double> list = new List<Double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 999, 1000 };
-            
-            /**
-             * 
-             */
-            Double one = 1;
-            
-            /**
-             *  Output 
-             */
-            Console.WriteLine("Execution " + list.GetType().Name + " array time: " + constantDouble.GetExecutionTime(list));
-        }
-    }
+			Console.WriteLine(new Constant<long>(array).ToString());
+		}
+
+		[TestMethod]
+		public void ListTest()
+		{
+			/**
+			 *  Test Double list 
+			 */
+			List<Double> list = new List<Double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 999, 1000 };
+			
+			/**
+			 * 
+			 */
+			Double one = 1;
+			
+			/**
+			 *  Output 
+			 */
+			Console.WriteLine("Execution " + list.GetType().Name + " array time: " + constantDouble.GetExecutionTime(list));
+		}
+	}
 }
