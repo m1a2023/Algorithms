3c9f0d6a94dcbfdc6ed9a2fac07cc6bcd06afe54
m1a2023 <mag2017go@gmail.com>
2024-09-21 16:05:15 +0500

    class Addition was done; added comments

--- Algorithms/models/Algorithms/models/Addition.cs
+++ Algorithms/models/Algorithms/models/Addition.cs
@@ -13,10 +13,32 @@ namespace Algorithms.models.Algorithms.models
     public class Addition<T> : Algorithm<T>
         where T : INumber<T>
     {
+        protected IList<T> Data { get; private set; }        
+        protected T Result { get; private set; }
+
+        /// <summary>Default constructor</summary>
+        public Addition() { } 
+
+
+		/// <summary>Extended constructor</summary>
+		/// <param name="Base"></param>
+		/// <param name="Exponent"></param>
+        public Addition(IList<T> Data)
+        {
+            this.Data = Data;
+            Result = Add(this.Data);
+        }
+        
+        
         /// <summary>General method that executes algorithm</summary>
         /// <param name="data">Value collection, implemented via ICollection</param>
         /// <returns>Variable the same type as the argument</returns>
         public override void Execute(IList<T> data)
+        {
+            Add(data);
+        }
+
+        private T Add(IList<T> data) 
         {
             T tmp = T.Zero;
 
@@ -25,9 +47,30 @@ namespace Algorithms.models.Algorithms.models
                 tmp += item;
             }
             
-            return;
+            return tmp;
         }
-        //public void Execute(T[] data) => Execute((ICollection<T>)data);
-        //public void Execute(List<T> data) => Execute((ICollection<T>)data);
+
+		/// <summary>Additional method for extended constructor</summary>
+		/// <returns>Execution algorithm time in milliseconds</returns>
+        public decimal GetExecutionTime()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew(); 
+
+            Execute(Data);
+
+            stopwatch.Stop();   
+
+            return new decimal (stopwatch.Elapsed.TotalMilliseconds);
+        }
+
+		/// <summary>String information representation</summary>
+		/// <returns>String of fields</returns>
+        public override string ToString()
+        {
+            string data = String.Join(", ", this.Data);
+            return $"Original collection: {data} [{Data.GetType()}]\n" +
+                $"Result: {Result} [{Result.GetType()}]\n" +
+                $"Execution time: {GetExecutionTime()}";
+        }
     }
 }
